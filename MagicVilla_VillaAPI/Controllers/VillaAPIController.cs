075a065a07ac59aafdd19a5ce208da97e26ea0df
user <user@WIN-91328OI5TCP>
2023-07-10 14:44:31 +0400

    Добавьте файлы проекта.

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,30 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    
+    //[Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+
+        [HttpGet("")]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{Id}")]
+        public ActionResult<VillaDTO> GetVilla(int Id)//all id should to be with equals literal,need => ID = ID;not id = ID 
+        {
+            if(Id == 0)return BadRequest();
+            var villa = VillaStore.villaList.FirstOrDefault(villa => villa.Id == Id);
+            if (villa == null) return NotFound();
+            return Ok(villa);
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaAPI.Models.DTO;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new ()
+        {
+            new VillaDTO {Id = 1, Name = "poool_view"},
+            new VillaDTO {Id = 2, Name = "beach_view"}
+        };
+    }
+}

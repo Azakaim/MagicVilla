690f027300c23a6771d72b43846ce70fb6505535
YuNikKel <romansergeev7680@gmail.com>
2023-07-21 12:56:30 +0400

    logging2

---
+++ MagicVilla_VillaAPI/Log/LoggingV2.cs
@@ -0,0 +1,31 @@
+﻿namespace MagicVilla_VillaAPI.Log
+{
+    public class LoggingV2:ILogging
+    {
+        public void Log(string message, string type)
+        {
+            if (type == "Error")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error - " + message);
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            else
+            {
+                if (type == "warning")
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Warning - " + message);
+                    Console.BackgroundColor = ConsoleColor.Black;
+
+                }
+                else
+                {
+                    Console.WriteLine(message);
+                }
+               
+            }
+        }
+    }
+}
+
